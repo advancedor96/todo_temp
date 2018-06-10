7f865b050d608a90b876714098dad9ca8b6a91fe
dingding <advancedor96@gmail.com>
2018-06-10 12:57:33 +0800

    把 _item.scss 的14行，display:inline-block 消掉，firstline的margin_top 就會對不準 。

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import solid_star from '@fortawesome/fontawesome-free-solid/faStar'
+import pencil from '@fortawesome/fontawesome-free-solid/faPencilAlt'
+
+
+import check from '@fortawesome/fontawesome-free-solid/faCheck'
+
+import empty_star from '@fortawesome/fontawesome-free-regular/faStar'
+
 class Item extends Component {
    render() {
       return (
          <div className="Item">
             <div className="first_line">
-               <input type="checkbox" defaultChecked={this.props.finished? true: false} />
-               <h3>{this.props.title}</h3>
-               <span className="star"></span>
-               <span className="pencil"></span>
+               <span className="checkbox">
+               {
+                  this.props.finished ?
+                  <FontAwesomeIcon icon={check} size="2x" style={{fontSize:'19px'}}/>
+                  : null
+               }
+                  
+               </span>
+               <span className="title">{this.props.title}</span>
+               <span className="star">
+               {
+                  this.props.favorite?
+                  <FontAwesomeIcon icon={solid_star} size="2x"   style={{fontSize:'24px'}}/> :
+                  <FontAwesomeIcon icon={empty_star} size="2x"  style={{fontSize:'24px'}}/>
+               }
+               </span>
+               <span className="pencil">
+                  <FontAwesomeIcon icon={pencil} size="2x"  style={{fontSize:'24px'}}/>
+               </span>
             </div>
             <div className="second_line">
                <span className="date"></span>
